df6d690a61b0fbbd224f2a6bd093f1aad6e5bfd9
AcelisWeaven <jeremy.graziani@gmail.com>
2018-10-01 21:47:42 +0200

    Refactor commands to avoid lot of if conditions

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 require('dotenv').config();
-const { Client, RichEmbed } = require('discord.js');
+const {Client, RichEmbed} = require('discord.js');
 const client = new Client();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-    const [cmd, ...args] = msg.toString().split(" ");
-    if (cmd === '/ping') {
+const commands = {
+    '/ping': (msg) => {
         msg.reply('Pong!');
-    }
-    if (cmd === '/help') {
+    },
+    '/help': (msg) => {
         const embed = new RichEmbed()
             .setTitle('Command list')
             .setColor(0x6666FF)
             .addField('/help', 'Displays this message');
         msg.channel.send(embed);
+    },
+    '/echo': (msg, ...args) => {
+        msg.channel.send(args.join(' '))
+    },
+};
+
+client.on('message', msg => {
+    const [cmd, ...args] = msg.toString().split(" ");
+    if (cmd in commands) {
+        commands[cmd](msg, ...args);
     }
 });
 
