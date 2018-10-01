92d606eb6f1a1685c9730e78d4e0716d092ad8a6
AcelisWeaven <jeremy.graziani@gmail.com>
2018-10-01 22:24:46 +0200

    Added /announce command

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 require('dotenv').config();
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
 const {Client, RichEmbed} = require('discord.js');
 const client = new Client();
+
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+db.defaults({
+    applications: [],
+    announce: "",
+}).write();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -21,11 +30,23 @@ const commands = {
         msg.channel.send(args.join(' '))
     },
     '/example-admin': (msg) => {
-        const adminRole = msg.guild.roles.find("name", "admin");
+        const adminRole = msg.guild.roles.find(r => r.name === "admin");
         if (msg.member.roles.has(adminRole.id)) {
             msg.reply("hey, you're an admin!");
         } else {
             msg.reply("hmm... looks like you're not an admin.");
+        }
+    },
+    '/announce': (msg, ...args) => {
+        const adminRole = msg.guild.roles.find(r => r.name === "admin");
+        if (args.length === 0) {
+            const embed = new RichEmbed()
+                .setTitle('Announce')
+                .setDescription(db.get('announce').value());
+            msg.channel.send(embed);
+        } else if (args.length > 0 && msg.member.roles.has(adminRole.id)) {
+            db.set('announce', args.join(' ')).write();
+            msg.channel.send("Announce updated.");
         }
     }
 };
