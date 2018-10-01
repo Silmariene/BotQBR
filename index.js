4cf99d32d69af3aec2081da14104659933f25ad8
AcelisWeaven <jeremy.graziani@gmail.com>
2018-10-02 00:07:38 +0200

    Applications are now managed in database, and submittable on a webpage

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const {Client, RichEmbed} = require('discord.js');
 const client = new Client();
+const express = require('express');
+const app = express();
 
 const adapter = new FileSync('db.json');
 const db = low(adapter);
@@ -11,8 +13,13 @@ db.defaults({
     announce: "",
 }).write();
 
+let applyChannel = null;
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    client.guilds.forEach(guild => {
+        let channel = guild.channels.find(ch => ch.name === 'test-acelis');
+        if (channel) applyChannel = channel;
+    });
 });
 
 const commands = {
@@ -56,14 +63,16 @@ const commands = {
         if (!user) {
             user = {
                 id: msg.author.id,
-                token: [...Array(32)].map(i=>(~~(Math.random()*36)).toString(36)).join(''),
-                status: 'empty'
+                tag: msg.author.tag,
+                token: [...Array(32)].map(i => (~~(Math.random() * 36)).toString(36)).join(''),
+                status: 'empty',
+                applyDate: Date.now(),
             };
             db.get('applications').push(user).write();
         }
 
         msg.member.createDM().then(channel => {
-            channel.send(`WIP: Here's your application form: ${user.token}`)
+            channel.send(`WIP: Here's your application form: ${process.env.PUBLIC_ENDPOINT}/apply?token=${user.token}`)
         })
     }
 };
@@ -82,3 +91,49 @@ client.on('message', msg => {
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
+
+// Website applications
+app.get('/apply', function (req, res) {
+    const [application] = db.get('applications').filter({token: req.query.token}).take(1).value();
+    if (!req.query.token || !application) {
+        res.status(404);
+        return res.send("Application not found");
+    }
+    res.send(`Hey, are you user ${application.tag}? Click <a href="/apply/validate?token=${application.token}">here</a> to submit your application.`);
+});
+
+app.get('/apply/validate', function (req, res) {
+    const applicationEntry = db.get('applications').filter({token: req.query.token}).take(1);
+    const [application] = applicationEntry.value();
+    if (!req.query.token || !application) {
+        res.status(404);
+        return res.send("Application not found");
+    }
+
+    if (application.status === 'empty') {
+        // first time the form is submitted, we handle it
+        applicationEntry.last().assign({
+            status: 'processing',
+            processingDate: Date.now(),
+        }).write();
+        applyChannel.send(`Somebody just applied! The name's ${application.tag}. Now... vote.`);
+        return res.send("Thanks for applying! We'll get in touch with you soon.");
+    }
+    if (application.status === 'processing') {
+        // we already are processing this application
+        return res.send("We already are processing this application. We'll get in touch with you soon.");
+    }
+    if (application.status === 'accepted') {
+        // we already accepted this application
+        return res.send("Congratz ;)");
+    }
+    if (application.status === 'accepted') {
+        // we already accepted this application
+        return res.send("Sorry, but your application have been refused.");
+    }
+
+    return res.send("Unknown status. This should not happen.");
+});
+
+app.listen(3000);
+console.log(`Server running on port 3000`);
